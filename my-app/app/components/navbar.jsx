1d94d96f627dd8c5a9eac5cfed08148c3ee8c297
yusra-aina choudhury <yusrachoudhury123@gmail.com>
2024-01-24 16:01:35 +0000

    nav bar

--- my-app/app/components/navbar.jsx
+++ my-app/app/components/navbar.jsx
@@ -2,29 +2,36 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <nav>
-      <ul>
-        <div className=" relative flex justify-center bg-red-600">
-          <Image
-            src="/mainlogo.png"
-            width={300}
-            height={300}
-            className="mx-auto"
-          />
-        </div>
-        <li>
-          <a href="/">About</a>
-        </li>
-        <li>
-          <a href="/about">Skills</a>
-        </li>
-        <li>
-          <a href="/about">Project</a>
-        </li>
-        <li>
-          <a href="/about">Contact</a>
-        </li>
-      </ul>
+    <nav className=" relative flex justify-center bg-red-600 gap-24">
+      <div className="bg-green-700 flex items-center ">
+        <a href="/" className="text-2xl">
+          About
+        </a>
+      </div>
+      <div className="bg-green-700 flex items-center ">
+        <a href="/" className="text-2xl">
+          Skills
+        </a>
+      </div>
+
+      <div>
+        <Image
+          src="/mainlogo.png"
+          width={300}
+          height={300}
+          className="mx-auto"
+        />
+      </div>
+      <div className="bg-green-700 flex items-center">
+        <a href="/" className="text-2xl">
+          Projects
+        </a>
+      </div>
+      <div className="bg-green-700 flex items-center">
+        <a href="/" className="text-2xl">
+          Contact
+        </a>
+      </div>
     </nav>
   );
 }
