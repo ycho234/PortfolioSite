55ea7368af10b32e021b9ab7843bff060b8b8267
yusra-aina choudhury <yusrachoudhury123@gmail.com>
2024-02-05 12:13:23 +0000

    editing

--- my-app/app/components/navbar.jsx
+++ my-app/app/components/navbar.jsx
@@ -1,17 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className=" relative flex justify-center gap-24">
+    <nav className=" relative flex justify-center gap-24 mx-10 my-2">
       <div className="flex items-center ">
         <a href="/" className="text-2xl hover:underline">
           About
         </a>
       </div>
       <div className="flex items-center ">
-        <a href="/" className="text-2xl hover:underline ">
-          Skills
-        </a>
+        <Link href="/about/page">
+          <p className="text-2xl hover:underline">Skills</p>
+        </Link>
       </div>
 
       <div>
@@ -19,7 +20,7 @@ export default function Navbar() {
           src="/mainlogo.png"
           width={300}
           height={300}
-          className="mx-auto"
+          className="mx-auto flex object-cover"
         />
       </div>
       <div className="flex items-center">
