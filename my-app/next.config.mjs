97c69e32b195bd8aea4318fd1fa637b4f70c3b1f
yusra-aina choudhury <yusrachoudhury123@gmail.com>
2024-02-06 11:31:20 +0000

    deployment test

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,9 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
-module.exports = {
+// export default nextConfig;
+// module.exports = {
+//   compiler: {
+//     styledComponents: true,
+//   },
+// };
+const nextConfig = {
   compiler: {
     styledComponents: true,
   },
 };
+
+export default nextConfig;
