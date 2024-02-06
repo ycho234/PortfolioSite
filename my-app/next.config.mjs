7a550f864d2157aec37a07716457760e36f4a8ac
yusra-aina choudhury <yusrachoudhury123@gmail.com>
2024-02-06 11:11:20 +0000

    testing

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,17 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
-module.exports = {
-  compiler: {
-    styledComponents: true,
-    type: module,
-  },
-};
-// const nextConfig = {
-//   compiler: {
-//     styledComponents: true,
-//   },
-// };
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
 // export default nextConfig;
+// module.exports = {
+//   compiler: {
+//     styledComponents: true,
+//     type: module,
+//   },
+// };
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+};
+
+export default nextConfig;
